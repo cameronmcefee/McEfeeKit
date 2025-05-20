686f553e7171a11bab66c3880ece3eb59d376d32
Cameron McEfee <cm@cameronmcefee.com>
2025-05-20 10:39:26 -0700

    clean up package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,21 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "McEfeeKit",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "McEfeeKit",
-            targets: ["McEfeeKit"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "McEfeeKit"),
-        .testTarget(
-            name: "McEfeeKitTests",
-            dependencies: ["McEfeeKit"]
-        ),
-    ]
+  name: "McEfeeKit",
+  products: [
+    .library(
+      name: "McEfeeKit",
+      targets: ["McEfeeKit"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "McEfeeKit"),
+    .testTarget(
+      name: "McEfeeKitTests",
+      dependencies: ["McEfeeKit"]
+    ),
+  ]
 )
