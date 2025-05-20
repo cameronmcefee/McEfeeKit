6a10c92fc233152cd6feb4b40e24445725964598
Cameron McEfee <cm@cameronmcefee.com>
2025-05-20 11:24:15 -0700

    Update Collection.swift

--- Sources/McEfeeKit/Extensions/Foundation/Collection.swift
+++ Sources/McEfeeKit/Extensions/Foundation/Collection.swift
@@ -2,4 +2,13 @@ public extension Collection where Indices.Iterator.Element == Index {
   subscript(safe index: Index) -> Iterator.Element? {
     return (startIndex <= index && index < endIndex) ? self[index] : nil
   }
+
+  func sorted<Value: Comparable>(
+    by keyPath: KeyPath<Element, Value>,
+    _ comparator: (Value, Value) -> Bool = (<)
+  ) -> [Element] {
+    return sorted {
+      comparator($0[keyPath: keyPath], $1[keyPath: keyPath])
+    }
+  }
 }
