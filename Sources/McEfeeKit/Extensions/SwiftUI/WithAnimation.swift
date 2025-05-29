aec5b9e3c2c76aa42646630fe55ab7c3bddf4254
Cameron McEfee <cm@cameronmcefee.com>
2025-05-29 12:14:06 -0700

    Create WithAnimation.swift

---
+++ Sources/McEfeeKit/Extensions/SwiftUI/WithAnimation.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+func withAnimation<T>(reduceMotion: Bool, _ animation: Animation = .default, _ body: () -> T) -> T {
+  if reduceMotion {
+    return body()
+  } else {
+    return SwiftUI.withAnimation(animation, body)
+  }
+}
